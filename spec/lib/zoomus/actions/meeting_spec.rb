4729372e41be3a4c109e9f602dd9f8e587803c84
Maxim Colls <collsmaxim@gmail.com>
2013-02-05 10:19:42 +0100

    Add meeting_list action spec

--- spec/lib/zoomus/actions/meeting_spec.rb
+++ spec/lib/zoomus/actions/meeting_spec.rb
@@ -1,10 +1,29 @@
 require 'spec_helper'
 
-describe Zoomus::Actions::User do
+describe Zoomus::Actions::Meeting do
 
   before :all do
     @zc = zoomus_client
   end
 
+  describe "#meeting_list action" do
+
+    before :each do
+      stub_request(:post, zoomus_url("/meeting/list")).to_return(:body => json_response("meeting_list"))
+    end
+
+    it "returns a hash" do
+      expect(@zc.meeting_list).to be_kind_of(Hash)
+    end
+
+    it "returns 'total_records'" do
+      expect(@zc.meeting_list["total_records"]).to eq(1)
+    end
+
+    it "returns 'meetings' Array" do
+      expect(@zc.meeting_list["meetings"]).to be_kind_of(Array)
+    end
+
+  end
 end
 
