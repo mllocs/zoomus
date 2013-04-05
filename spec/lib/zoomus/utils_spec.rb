cde0be623c6698f9ff4601e94282e73a3886711d
Maxim Colls <collsmaxim@gmail.com>
2013-04-05 11:33:11 +0200

    Added Utils specs file

---
+++ spec/lib/zoomus/utils_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Zoomus::Utils do
+
+  describe "#argument_error"
+
+  describe "#raise_if_error!"
+
+  describe "#parse_response"
+
+  describe "#require_params"
+
+  describe "#extract_options!"
+
+end
