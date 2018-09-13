8ac46c3715cc05326ac9e424aa912a291ef57af5
Kyle Boe <ksboe1@gmail.com>
2018-09-13 07:51:21 -0700

    add blanks for account spec

--- lib/zoom/actions/account.rb
+++ lib/zoom/actions/account.rb
@@ -3,6 +3,91 @@
 module Zoom
   module Actions
     module Account
+      def account_list(*args)
+        # TODO: implement accounts_list
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'accounts_list is not yet implemented'
+      end
+
+      def account_create(*args)
+        # TODO: implement accounts_create
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'accounts_create is not yet implemented'
+      end
+
+      def account_get(*args)
+        # TODO: implement account_get
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'account_get is not yet implemented'
+      end
+
+      def account_delete(*args)
+        # TODO: implement account_delete
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'account_delete is not yet implemented'
+      end
+
+      def account_options_update(*args)
+        # TODO: implement account_options_update
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'account_options_update is not yet implemented'
+      end
+
+      def account_settings_get(*args)
+        # TODO: implement account_settings_get
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'account_settings_get is not yet implemented'
+      end
+
+      def account_settings_update(*args)
+        # TODO: implement account_settings_update
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'account_settings_update is not yet implemented'
+      end
+
+      # Billing related API Endpoints
+
+      def account_billing_get(*args)
+        # TODO: implement account_billing_get
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'account_billing_get is not yet implemented'
+      end
+
+      def account_billing_update(*args)
+        # TODO: implement account_billing_update
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'account_billing_update is not yet implemented'
+      end
+
+      def account_plans_list(*args)
+        # TODO: implement account_plans_list
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'account_plans_list is not yet implemented'
+      end
+
+      def some_method(*args)
+        # TODO: implement some_method
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'some_method is not yet implemented'
+      end
+
+      def some_method(*args)
+        # TODO: implement some_method
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'some_method is not yet implemented'
+      end
+
+      def some_method(*args)
+        # TODO: implement some_method
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'some_method is not yet implemented'
+      end
+
+      def some_method(*args)
+        # TODO: implement some_method
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'some_method is not yet implemented'
+      end
     end
   end
 end
