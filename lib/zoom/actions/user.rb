bae20168beab1ffed432ddebbf63ef9389677af0
Kyle Boe <ksboe1@gmail.com>
2018-09-13 07:51:55 -0700

    WIP build user endpoints

--- lib/zoom/actions/user.rb
+++ lib/zoom/actions/user.rb
@@ -5,68 +5,136 @@ module Zoom
     module User
       def user_list(*args)
         options = Utils.extract_options!(args)
-        Utils.parse_response self.class.post('/user/list', query: options)
-      end
-
-      def user_pending(*args)
-        options = Utils.extract_options!(args)
-        Utils.parse_response self.class.post('/user/pending', query: options)
+        Utils.permit_params(%i[status page_size page_number], options)
+        Utils.parse_response self.class.get('/users', query: options.merge(access_token: access_token))
       end
 
       def user_create(*args)
         options = Utils.extract_options!(args)
-        Utils.require_params(%i[type email], options)
-        Utils.parse_response self.class.post('/user/create', query: options)
-      end
-
-      def user_delete(*args)
-        options = Utils.extract_options!(args)
-        Utils.require_params([:id], options)
-        Utils.parse_response self.class.post('/user/delete', query: options)
-      end
-
-      def user_permanent_delete(*args)
-        options = Utils.extract_options!(args)
-        Utils.require_params([:id], options)
-        Utils.parse_response self.class.post('/user/permanentdelete', query: options)
-      end
-
-      def user_custcreate(*args)
-        options = Utils.extract_options!(args)
-        Utils.require_params(%i[type email], options)
-        Utils.parse_response self.class.post('/user/custcreate', query: options)
-      end
-
-      def user_update(*args)
-        options = Utils.extract_options!(args)
-        Utils.require_params([:id], options)
-        Utils.parse_response self.class.post('/user/update', query: options)
+        Utils.require_params(%i[email type first_name last_name password], options)
+        Utils.parse_response self.class.post('/users', body: { action: 'create', user_info: options }, query: { access_token: access_token }))
       end
 
       def user_get(*args)
         options = Utils.extract_options!(args)
         Utils.require_params([:id], options)
-        Utils.parse_response self.class.post('/user/get', query: options)
+        Utils.permit_params(%i[id login_type], options)
+        Utils.parse_response self.class.get("/users/#{options[:id]}", query: options.merge(access_token: access_token))
       end
 
-      def user_getbyemail(*args)
+      def user_update(*args)
         options = Utils.extract_options!(args)
-        Utils.require_params([:email], options)
-        Utils.parse_response self.class.post('/user/getbyemail', query: options)
+        Utils.permit_params(%i[first_name last_name type pmi timezone dept vanity_name host_key cms_user_id], options)
+        Utils.parse_response self.class.patch('/users', body: options, query: { access_token: access_token })
       end
 
-      def user_autocreate(*args)
+      def user_delete(*args)
         options = Utils.extract_options!(args)
-        Utils.require_params(%i[type email password], options)
-        Utils.parse_response self.class.post('/user/autocreate', query: options)
+        Utils.require_params([:id], options)
+        Utils.permit_params(%i[action transfer_email transfer_meeting transfer_webinar transfer_recording], options)
+        Utils.parse_response self.class.delete("/users/#{options.slice!(:id)}", query: options.merge(access_token: access_token))
       end
 
-      # Need to contact zoom support to enable autocreate2 on your account
-      # Behaves like autocreate, but users email address does not have to match managed domain
-      def user_autocreate2(*args)
-        options = Utils.extract_options!(args)
-        Utils.require_params(%i[type email password], options)
-        Utils.parse_response self.class.post('/user/autocreate2', query: options)
+      def user_assistants_list(*args)
+        # TODO: implement user_assistants_list
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_assistants_list is not yet implemented'
+      end
+
+      def user_assistants_create(*args)
+        # TODO: implement user_assistants_create
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_assistants_create is not yet implemented'
+      end
+
+      def user_assistants_delete_all(*args)
+        # TODO: implement user_assistants_delete_all
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_assistants_delete_all is not yet implemented'
+      end
+
+      def user_assistants_delete(*args)
+        # TODO: implement user_assistants_delete
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_assistants_delete is not yet implemented'
+      end
+
+      def user_schedulers_list(*args)
+        # TODO: implement user_schedulers_list
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_schedulers_list is not yet implemented'
+      end
+
+      def user_schedulers_delete_all(*args)
+        # TODO: implement user_schedulers_delete_all
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_schedulers_delete_all is not yet implemented'
+      end
+
+      def user_schedulers_delete(*args)
+        # TODO: implement user_schedulers_delete
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_schedulers_delete is not yet implemented'
+      end
+
+      def user_upload_picture(*args)
+        # TODO: implement user_upload_picture
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_upload_picture is not yet implemented'
+      end
+
+      def user_settings_get(*args)
+        # TODO: implement user_settings_get
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_settings_get is not yet implemented'
+      end
+
+      def user_settings_update(*args)
+        # TODO: implement user_settings_update
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_settings_update is not yet implemented'
+      end
+
+      def user_status_update(*args)
+        # TODO: implement user_status_update
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_status_update is not yet implemented'
+      end
+
+      def user_password_update(*args)
+        # TODO: implement user_password_update
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_password_update is not yet implemented'
+      end
+
+      def user_permissions_get(*args)
+        # TODO: implement user_permissions_get
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_permissions_get is not yet implemented'
+      end
+
+      def user_token_get(*args)
+        # TODO: implement user_token_get
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_token_get is not yet implemented'
+      end
+
+      def user_token_delete(*args)
+        # TODO: implement user_token_delete
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_token_delete is not yet implemented'
+      end
+
+      def user_email_check(*args)
+        # TODO: implement user_email_check
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_email_check is not yet implemented'
+      end
+
+      def user_vanity_name_check(*args)
+        # TODO: implement user_vanity_name_check
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'user_vanity_name_check is not yet implemented'
       end
 
       Utils.define_bang_methods(self)
