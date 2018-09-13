250dfd8cca6f730f84d9202b9250d371f894199b
Kyle Boe <ksboe1@gmail.com>
2018-09-13 07:52:33 -0700

    begin webinar action migration

--- lib/zoom/actions/webinar.rb
+++ lib/zoom/actions/webinar.rb
@@ -5,41 +5,89 @@ module Zoom
     module Webinar
       def webinar_list(*args)
         options = Utils.extract_options!(args)
-        Utils.require_params(:host_id, options)
+        Utils.require_params(:user_id, options)
         Utils.process_datetime_params!(:start_time, options)
-        Utils.parse_response self.class.post('/webinar/list', query: options)
+        Utils.parse_response self.class.get("/users/#{options[:user_id]}/webinars", query: options)
       end
 
       def webinar_create(*args)
         options = Utils.extract_options!(args)
-        Utils.require_params(%i[host_id topic], options)
+        Utils.require_params(%i[user_id topic], options)
         Utils.process_datetime_params!(:start_time, options)
-        Utils.parse_response self.class.post('/webinar/create', query: options)
-      end
-
-      def webinar_update(*args)
-        options = Utils.extract_options!(args)
-        Utils.require_params(%i[id host_id], options)
-        Utils.process_datetime_params!(:start_time, options)
-        Utils.parse_response self.class.post('/webinar/update', query: options)
-      end
-
-      def webinar_delete(*args)
-        options = Utils.extract_options!(args)
-        Utils.require_params(%i[id host_id], options)
-        Utils.parse_response self.class.post('/webinar/delete', query: options)
-      end
-
-      def webinar_end(*args)
-        options = Utils.extract_options!(args)
-        Utils.require_params(%i[id host_id], options)
-        Utils.parse_response self.class.post('/webinar/end', query: options)
+        Utils.parse_response self.class.post("/users/#{options[:user_id]}/webinars", query: options)
       end
 
       def webinar_get(*args)
         options = Utils.extract_options!(args)
-        Utils.require_params(%i[id host_id], options)
-        Utils.parse_response self.class.post('/webinar/get', query: options)
+        Utils.require_params([:id], options)
+        Utils.parse_response self.class.get("/webinars/#{options[:id]}", query: options)
+      end
+
+      def webinar_update(*args)
+        options = Utils.extract_options!(args)
+        Utils.require_params([:id], options)
+        Utils.process_datetime_params!(:start_time, options)
+        Utils.parse_response self.class.post("/webinars/#{options[:id]}", query: options)
+      end
+
+      def webinar_delete(*args)
+        options = Utils.extract_options!(args)
+        Utils.require_params([:id], options)
+        Utils.parse_response self.class.delete("/webinars/#{options[:id]}", query: options)
+      end
+
+      def webinar_status_update(*args)
+        options = Utils.extract_options!(args)
+        Utils.require_params(%i[id status], options)
+        Utils.parse_response self.class.post("/webinars/#{options[:id]}/status}", query: options)
+      end
+
+      def webinar_panelists_list(*args)
+        # TODO: implement webinar_panelists_list
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'webinar_panelists_list is not yet implemented'
+      end
+
+      def webinar_panelist_add(*args)
+        # TODO: implement webinar_panelist_add
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'webinar_panelist_add is not yet implemented'
+      end
+
+      def webinar_panelists_delete_all(*args)
+        # TODO: implement webinar_panelists_delete_all
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'webinar_panelists_delete_all is not yet implemented'
+      end
+
+      def webinar_panelist_delete(*args)
+        # TODO: implement webinar_panelist_delete
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'webinar_panelist_delete is not yet implemented'
+      end
+
+      def webinar_registrants_list(*args)
+        # TODO: implement webinar_registrants_list
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'webinar_registrants_list is not yet implemented'
+      end
+
+      def webinar_registrant_add(*args)
+        # TODO: implement webinar_registrant_add
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'webinar_registrant_add is not yet implemented'
+      end
+
+      def webinar_registrants_status_update(*args)
+        # TODO: implement webinar_registrants_status_update
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'webinar_registrants_status_update is not yet implemented'
+      end
+
+      def past_webinar_list(*args)
+        # TODO: implement past_webinars_list
+        # options = Utils.extract_options!(args)
+        raise Zoom::NotImplemented, 'past_webinars_list is not yet implemented'
       end
 
       Utils.define_bang_methods(self)
