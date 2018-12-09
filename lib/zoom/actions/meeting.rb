d7e5a0126b4ddcbe17ae9275c8847675ee33c6e8
Jesús Eduardo Clemens Chong <edu.clemens@gmail.com>
2018-12-09 01:20:55 -0700

    Testing result

--- lib/zoom/actions/meeting.rb
+++ lib/zoom/actions/meeting.rb
@@ -5,29 +5,30 @@ module Zoom
     module Meeting
       # List all the scheduled meetings on Zoom.
       def meeting_list(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args)).require(%i[user_id])
-        # TODO Handle `page_size` attr, Defaults to 30. Max of 300 meetings.
-        # TODO Handle `page_number` attr, Defaults to 1.
-        ["/users/#{options[:user_id]}/meetings", query: options.except(:user_id), headers: request_headers]
-        # Utils.parse_response self.class.get("/users/#{options[:user_id]}/meetings", query: options.except(:user_id), headers: request_headers)
+        options = Zoom::Params.new(Utils.extract_options!(args))
+        options.require(%i[user_id])
+        Utils.parse_response self.class.get("/users/#{options[:user_id]}/meetings", query: options.except(:user_id), headers: request_headers)
       end
 
       # Create a meeting on Zoom, return the created meeting URL
       def meeting_create(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args)).require(%i[user_id])
+        options = Zoom::Params.new(Utils.extract_options!(args))
+        options.require(%i[user_id])
         Utils.process_datetime_params!(:start_time, options)
         Utils.parse_response self.class.post("/users/#{options[:user_id]}/meetings", body: options.except(:user_id).to_json, headers: request_headers)
       end
 
       # Get a meeting on Zoom via meeting ID, return the meeting info.
       def meeting_get(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args)).require(%i[meeting_id])
+        options = Zoom::Params.new(Utils.extract_options!(args))
+        options.require(%i[meeting_id])
         self.class.get("/meetings/#{options[:meeting_id]}", headers: request_headers)
       end
 
       # Update meeting info on Zoom via meeting ID.
       def meeting_update(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args)).require(%i[meeting_id])
+        options = Zoom::Params.new(Utils.extract_options!(args))
+        options.require(%i[meeting_id])
         Utils.process_datetime_params!(:start_time, options)
         # TODO Handle `topic` attr, Max of 300 characters.
         # TODO Handle `timezone` attr, refer to the id value in timezone list JSON file. like "America/Los_Angeles"
@@ -39,13 +40,15 @@ module Zoom
 
       # Delete a meeting on Zoom, return the deleted meeting ID.
       def meeting_delete(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args)).require(%i[meeting_id])
+        options = Zoom::Params.new(Utils.extract_options!(args))
+        options.require(%i[meeting_id])
         Utils.parse_response self.class.delete("/meetings/#{options[:meeting_id]}", query: options.except(:meeting_id), headers: request_headers)
       end
 
       # Update a meeting's status
       def meeting_update_status(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args)).require(%i[meeting_id])
+        options = Zoom::Params.new(Utils.extract_options!(args))
+        options.require(%i[meeting_id])
         Utils.parse_response self.class.patch("/meetings/#{options[:meeting_id]}/status", body: options.except(:meeting_id).to_json, headers: request_headers)
       end
 
@@ -63,7 +66,8 @@ module Zoom
 
       # Register for a meeting.
       def meeting_register(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args)).require(%i[meeting_id email first_name last_name])
+        options = Zoom::Params.new(Utils.extract_options!(args))
+        options.require(%i[meeting_id email first_name last_name])
         Utils.parse_response self.class.post("/meetings/#{options[:meeting_id]}/registrants", body: options.except(:meeting_id).to_json, headers: request_headers)
       end
     end
