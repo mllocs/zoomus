641db6496470efc86cc6901229a2fe43f3e6a211
Chris Dahms <zentrification@gmail.com>
2020-03-26 13:51:23 -0400

    fixed v2 metrics for crc & meetings (details & participants)

--- spec/lib/zoom/actions/metrics/meetings_spec.rb
+++ spec/lib/zoom/actions/metrics/meetings_spec.rb
@@ -2,60 +2,62 @@
 
 require 'spec_helper'
 
-xdescribe Zoom::Actions::Metrics do
+describe Zoom::Actions::Metrics do
+  let(:zc) { zoom_client }
+  let(:args) { { type: 1, from: '2013-04-05T15:50:47Z', to: '2013-04-09T19:00:00Z' } }
+  let(:response) { zc.metrics_meetings(args) }
 
-  before :all do
-    @zc = zoom_client
-    @args = { type: 1,
-              from: '2013-04-05T15:50:47Z',
-              to: '2013-04-09T19:00:00Z' }
+  describe '#metrics_meetings action' do
+    context 'with 200 response' do
+      before :each do
+        stub_request(
+          :get,
+          zoom_url('/metrics/meetings')
+        ).to_return(status: 200,
+                    body: json_response('metrics','meetings'),
+                    headers: { 'Content-Type' => 'application/json' })
+      end
+
+      it "requires a 'from' argument" do
+        expect { zc.metrics_meetings(filter_key(args, :from)) }.to raise_error(Zoom::ParameterMissing)
+      end
+
+      it "requires a 'to' argument" do
+        expect { zc.metrics_meetings(filter_key(args, :to)) }.to raise_error(Zoom::ParameterMissing)
+      end
+
+      it 'returns a hash' do
+        expect(zc.metrics_meetings(args)).to be_kind_of(Hash)
+      end
+
+      it "returns 'next_page_token'" do
+        expect(response['next_page_token']).to be_kind_of(String)
+      end
+
+      it "returns 'total_records'" do
+        expect(response['total_records']).to eq(1)
+      end
+
+      it "returns 'meetings' Array" do
+        expect(response['meetings']).to be_kind_of(Array)
+      end
+    end
+
+    context 'with 300 response' do
+      before :each do
+        stub_request(
+          :get,
+          zoom_url('/metrics/meetings')
+        ).to_return(status: 300,
+                    body: '{ "code": 300, "message": "Can not access webinar info, {meetingId}" }',
+                    headers: { 'Content-Type' => 'application/json' })
+      end
+
+      it 'raises an error' do
+        expect { response }.to raise_error(Zoom::Error)
+      end
+    end
+
   end
 
-  xdescribe '#metrics_meetings action' do
-    before :each do
-      stub_request(
-        :post,
-        zoom_url('/metrics/meetings')
-      ).to_return(body: json_response('metrics_meetings'))
-    end
-
-    it "requires a 'type' argument" do
-      expect { @zc.metrics_meetings(filter_key(@args, :type)) }.to raise_error(ArgumentError)
-    end
-
-    it "requires a 'from' argument" do
-      expect { @zc.metrics_meetings(filter_key(@args, :from)) }.to raise_error(ArgumentError)
-    end
-
-    it "requires a 'to' argument" do
-      expect { @zc.metrics_meetings(filter_key(@args, :to)) }.to raise_error(ArgumentError)
-    end
-
-    it 'returns a hash' do
-      expect(@zc.metrics_meetings(@args)).to be_kind_of(Hash)
-    end
-
-    it "returns 'total_records'" do
-      expect(@zc.metrics_meetings(@args)['total_records']).to eq(1)
-    end
-
-    it "returns 'meetings' Array" do
-      expect(@zc.metrics_meetings(@args)['meetings']).to be_kind_of(Array)
-    end
-  end
-
-  xdescribe '#metrics_meetings! action' do
-    before :each do
-      stub_request(
-        :post,
-        zoom_url('/metrics/meetings')
-      ).to_return(body: json_response('error'))
-    end
-
-    it 'raises Zoom::Error exception' do
-      expect {
-        @zc.metrics_meetings!(@args)
-      }.to raise_error(Zoom::Error)
-    end
-  end
 end
