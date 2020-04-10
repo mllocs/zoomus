a2832637a9ff6e08d1bd7b5974d2f40d7f542268
Alexandr Kalinka <alex.kalinoff@gmail.com>
2020-04-10 21:41:41 +0300

    add Zoom::Actions::User#user_assistants_delete user_assistants_delete_all

--- spec/lib/zoom/actions/user/assistant/delete_all_spec.rb
+++ spec/lib/zoom/actions/user/assistant/delete_all_spec.rb
@@ -4,31 +4,25 @@ require 'spec_helper'
 
 describe Zoom::Actions::User do
   let(:zc) { zoom_client }
-  let(:args) { { user_id: 'eIimBAXqSrWOcB_EOIXTog', assistant_id: '555' } }
+  let(:args) { { user_id: 'eIimBAXqSrWOcB_EOIXTog' } }
 
-  describe '#user_assistants_delete' do
+  describe '#user_assistants_delete_all' do
     context 'with a 204 response' do
       before :each do
         stub_request(
           :delete,
-          zoom_url("/users/#{args[:user_id]}/assistants/#{args[:assistant_id]}")
+          zoom_url("/users/#{args[:user_id]}/assistants")
         ).to_return(status: 204)
       end
 
       it 'requires user_id param' do
         expect do
-          zc.user_assistants_delete(filter_key(args, :user_id))
+          zc.user_assistants_delete_all(filter_key(args, :user_id))
         end.to raise_error(Zoom::ParameterMissing, [:user_id].to_s)
       end
 
-      it 'requires assistant_id param' do
-        expect do
-          zc.user_assistants_delete(filter_key(args, :assistant_id))
-        end.to raise_error(Zoom::ParameterMissing, [:assistant_id].to_s)
-      end
-
       it 'returns the http status code as a number' do
-        expect(zc.user_assistants_delete(args)).to eql(204)
+        expect(zc.user_assistants_delete_all(args)).to eql(204)
       end
     end
 
@@ -36,14 +30,14 @@ describe Zoom::Actions::User do
       before :each do
         stub_request(
           :delete,
-          zoom_url("/users/#{args[:user_id]}/assistants/#{args[:assistant_id]}")
+          zoom_url("/users/#{args[:user_id]}/assistants")
         ).to_return(status: 404,
                     body: json_response('error', 'not_found'),
                     headers: { 'Content-Type' => 'application/json' })
       end
 
       it 'raises an error' do
-        expect { zc.user_assistants_delete(args) }.to raise_error(Zoom::Error)
+        expect { zc.user_assistants_delete_all(args) }.to raise_error(Zoom::Error)
       end
     end
   end
