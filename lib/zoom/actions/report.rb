f0abc1cbf14f900d47d5c5288f697988f75b5aec
Kyle Boe <ksboe1@gmail.com>
2020-04-15 14:10:36 -0700

    raise not implemented errors for not yet implemented report endpoints

--- lib/zoom/actions/report.rb
+++ lib/zoom/actions/report.rb
@@ -11,26 +11,32 @@ module Zoom
 
       def hosts_report(*args)
         # TODO: implement hosts_report
+        raise Zoom::NotImplemented, 'hosts_report is not yet implemented'
       end
 
       def meetings_report(*args)
         # TODO: implement meetings_report
+        raise Zoom::NotImplemented, 'meetings_report is not yet implemented'
       end
 
       def meeting_details_report(*args)
         # TODO: implement meeting_details_report
+        raise Zoom::NotImplemented, 'meeting_details_report is not yet implemented'
       end
 
       def meeting_participants_report(*args)
         # TODO: implement meeting_participants_report
+        raise Zoom::NotImplemented, 'meeting_participants_report is not yet implemented'
       end
 
       def meeting_polls_report(*args)
         # TODO: implement meeting_polls_report
+        raise Zoom::NotImplemented, 'meeting_polls_report is not yet implemented'
       end
 
       def webinar_details_report(*args)
         # TODO: implement webinar_details_report
+        raise Zoom::NotImplemented, 'webinar_details_report is not yet implemented'
       end
 
       def webinar_participants_report(*args)
@@ -40,15 +46,18 @@ module Zoom
       end
 
       def webinar_polls_report(*args)
-        # TODO: implement report_hosts
+        # TODO: implement webinar_polls_report
+        raise Zoom::NotImplemented, 'webinar_polls_report is not yet implemented'
       end
 
       def webinar_qa_report(*args)
-        # TODO: implement report_hosts
+        # TODO: implement webinar_qa_report
+        raise Zoom::NotImplemented, 'webinar_qa_report is not yet implemented'
       end
 
       def telephone_report(*args)
-        # TODO: implement report_hosts
+        # TODO: implement telephone_report
+        raise Zoom::NotImplemented, 'telephone_report is not yet implemented'
       end
     end
   end
