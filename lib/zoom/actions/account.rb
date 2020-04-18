d4e29f63e3f1ce6ecb3515722b07e32f7ac00301
Kyle Boe <ksboe1@gmail.com>
2020-04-17 19:41:19 -0700

    WIP account updates

--- lib/zoom/actions/account.rb
+++ lib/zoom/actions/account.rb
@@ -5,63 +5,63 @@ module Zoom
     module Account
       def account_list(*args)
         params = Zoom::Params.new(Utils.extract_options!(args))
-        params.permit(:page_size, :page_number)
-        Utils.parse_response self.class.get("/accounts", query: params, headers: request_headers)
+        params.permit(%i[page_size page_number])
+        Utils.parse_response self.class.get('/accounts', query: params, headers: request_headers)
       end
 
       def account_create(*args)
-        # TODO: implement accounts_create
-        # options = Utils.extract_options!(args)
-        raise Zoom::NotImplemented, 'accounts_create is not yet implemented'
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(%i[first_name last_name email password]).permit(options: %i[share_rc room_connectors share_mc meeting_connectors pay_mode])
+        Utils.parse_response self.class.post('/accounts', body: params, headers: request_headers)
       end
 
       def account_get(*args)
-        # TODO: implement account_get
-        # options = Utils.extract_options!(args)
-        raise Zoom::NotImplemented, 'account_get is not yet implemented'
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:account_id)
+        Utils.parse_response self.class.get("/accounts/#{params[:account_id]}", headers: request_headers)
       end
 
       def account_delete(*args)
-        # TODO: implement account_delete
-        # options = Utils.extract_options!(args)
-        raise Zoom::NotImplemented, 'account_delete is not yet implemented'
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:account_id)
+        Utils.parse_response self.class.delete("/accounts/#{params[:account_id]}", headers: request_headers)
       end
 
       def account_options_update(*args)
-        # TODO: implement account_options_update
-        # options = Utils.extract_options!(args)
-        raise Zoom::NotImplemented, 'account_options_update is not yet implemented'
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:account_id).permit(%i[share_rc room_connectors share_mc meeting_connectors pay_mode])
+        Utils.parse_response self.class.patch("/accounts/#{params[:account_id]}/options", body: params.except(:account_id), headers: request_headers)
       end
 
       def account_settings_get(*args)
         params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:id)
-        Utils.parse_response self.class.get("/accounts/#{params[:id]}/settings", query: params.except(:id), headers: request_headers)
+        params.require(:account_id).permit(:option)
+        Utils.parse_response self.class.get("/accounts/#{params[:account_id]}/settings", query: params.except(:account_id), headers: request_headers)
       end
 
       def account_settings_update(*args)
-        # TODO: implement account_settings_update
-        # options = Utils.extract_options!(args)
-        raise Zoom::NotImplemented, 'account_settings_update is not yet implemented'
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:account_id).permit(:option, Zoom::Constants::Account::Settings::PERMITTED_KEYS)
+        Utils.parse_response self.class.patch("/accounts/#{params[:account_id]}/settings", query: params.slice(:option), body: params.except(%i[account_id option]), headers: request_headers)
       end
 
       # Billing related API Endpoints
 
       def account_billing_get(*args)
         # TODO: implement account_billing_get
-        # options = Utils.extract_options!(args)
+        # params = Zoom::Params.new(Utils.extract_options!(args))
         raise Zoom::NotImplemented, 'account_billing_get is not yet implemented'
       end
 
       def account_billing_update(*args)
         # TODO: implement account_billing_update
-        # options = Utils.extract_options!(args)
+        # params = Zoom::Params.new(Utils.extract_options!(args))
         raise Zoom::NotImplemented, 'account_billing_update is not yet implemented'
       end
 
       def account_plans_list(*args)
         # TODO: implement account_plans_list
-        # options = Utils.extract_options!(args)
+        # params = Zoom::Params.new(Utils.extract_options!(args))
         raise Zoom::NotImplemented, 'account_plans_list is not yet implemented'
       end
     end
