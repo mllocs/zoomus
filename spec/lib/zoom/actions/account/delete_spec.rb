705d76449d63fb2b1e36532c8ecbe06fdf097b1c
Kyle Boe <ksboe1@gmail.com>
2020-04-21 19:55:10 -0700

    add account delete specs and fixtures

---
+++ spec/lib/zoom/actions/account/delete_spec.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Zoom::Actions::Account do
+  let(:zc) { zoom_client }
+  let(:args) { { account_id: 1 } }
+
+  describe '#account_delete action' do
+    context 'with a valid response' do
+      before :each do
+        stub_request(
+          :delete,
+          zoom_url("/accounts/#{args[:account_id]}")
+        ).to_return(status: 204,
+                    body: json_response('account', 'delete'),
+                    headers: { 'Content-Type' => 'application/json' })
+      end
+
+      it 'requires the account_id params' do
+        expect { zc.account_delete }.to raise_error(Zoom::ParameterMissing, [:account_id].to_s)
+      end
+
+      it 'returns the http status code as a number' do
+        expect(zc.account_delete(args)).to eql(204)
+      end
+    end
+
+    context 'with a 4xx response' do
+      before :each do
+        stub_request(
+          :delete,
+          zoom_url("/accounts/#{args[:account_id]}")
+        ).to_return(status: 400,
+                    body: json_response('error', 'not_found'),
+                    headers: { 'Content-Type' => 'application/json' })
+      end
+
+      it 'raises Zoom::Error exception' do
+        expect { zc.account_delete(args) }.to raise_error(Zoom::Error)
+      end
+    end
+  end
+end
