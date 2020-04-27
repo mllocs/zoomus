9d32753f0daa1936d1452a427cb69fd0d75f5be6
Błażej Kotowski <kotowski.blazej@gmail.com>
2020-04-27 18:13:38 +0200

    Naming

--- lib/zoom/actions/roles.rb
+++ lib/zoom/actions/roles.rb
@@ -9,9 +9,9 @@ module Zoom
       end
 
       def roles_create(*args)
-        options = Params.new(Utils.extract_options!(args))
-        options.require(:name).permit(%i[description privileges])
-        Utils.parse_response self.class.post("/roles", body: options.to_json, headers: request_headers)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:name).permit(%i[description privileges])
+        Utils.parse_response self.class.post("/roles", body: params.to_json, headers: request_headers)
       end
 
       def roles_members(*args)
@@ -21,9 +21,9 @@ module Zoom
       end
 
       def roles_assign(*args)
-        options = Params.new(Utils.extract_options!(args))
-        options.require(%i[role_id members])
-        Utils.parse_response self.class.post("/roles/#{options[:role_id]}/members", body: options.except(:role_id).to_json, headers: request_headers)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(%i[role_id members])
+        Utils.parse_response self.class.post("/roles/#{params[:role_id]}/members", body: params.except(:role_id).to_json, headers: request_headers)
       end
 
       def roles_unassign(*args)
