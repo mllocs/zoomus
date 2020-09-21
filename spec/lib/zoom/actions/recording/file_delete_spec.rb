5d28b456633c398aaafb5de15340e8eabd56091b
Kyle Boe <ksboe1@gmail.com>
2020-09-21 16:28:04 -0700

    rename spec file to match method name change

--- spec/lib/zoom/actions/recording/file_delete_spec.rb
+++ spec/lib/zoom/actions/recording/file_delete_spec.rb
@@ -6,7 +6,7 @@ describe Zoom::Actions::Recording do
   let(:zc) { zoom_client }
   let(:args) { { recording_id: 'kEFomHcIRgqxZT8D086O6A', meeting_id: 933560800 } }
 
-  describe '#recording_delete action' do
+  describe '#meeting_recording_file_delete action' do
     before :each do
       stub_request(
         :delete,
@@ -19,23 +19,15 @@ describe Zoom::Actions::Recording do
 
     it "requires a 'meeting_id' and 'recording_id' argument" do
       expect {
-        zc.recording_delete(filter_key(args, :meeting_id))
+        zc.meeting_recording_file_delete(filter_key(args, :meeting_id))
       }.to raise_error(Zoom::ParameterMissing)
       expect {
-        zc.recording_delete(filter_key(args, :recording_id))
+        zc.meeting_recording_file_delete(filter_key(args, :recording_id))
       }.to raise_error(Zoom::ParameterMissing)
     end
 
     it 'returns a status code' do
-      expect(zc.recording_delete(args)).to eq 204
-    end
-  end
-
-  describe '#recording_delete! action' do
-    it 'raises NoMethodError exception' do
-      expect {
-        zc.recording_delete!(args)
-      }.to raise_error(NoMethodError)
+      expect(zc.meeting_recording_file_delete(args)).to eq 204
     end
   end
 end
