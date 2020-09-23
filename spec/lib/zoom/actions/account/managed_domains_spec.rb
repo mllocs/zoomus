713e84ddfc421b0d363116c4128f7bbeeec30c31
Kyle Boe <ksboe1@gmail.com>
2020-09-23 09:34:32 -0700

    add spec file and fixtures for managed_domains - #60

---
+++ spec/lib/zoom/actions/account/managed_domains_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Zoom::Actions::Account do
+  let(:zc) { zoom_client }
+  let(:args) { { account_id: 1 } }
+
+  describe '#account_managed_domains action' do
+    context 'with a valid response' do
+      before :each do
+        stub_request(
+          :get,
+          zoom_url("/accounts/#{args[:account_id]}/managed_domains")
+        ).to_return(body: json_response('account', 'managed_domains'),
+                    headers: { 'Content-Type' => 'application/json' })
+      end
+
+      it 'returns a hash' do
+        expect(zc.account_managed_domains(args)).to be_kind_of(Hash)
+      end
+
+      it 'raises an error when missing a required param' do
+        expect { zc.account_managed_domains }.to raise_error(Zoom::ParameterMissing, [:account_id].to_s)
+      end
+    end
+
+    context 'with a 4xx response' do
+      before :each do
+        stub_request(
+          :get,
+          zoom_url("/accounts/#{args[:account_id]}/managed_domains")
+        ).to_return(status: 400,
+                    body: json_response('error', 'not_found'),
+                    headers: { 'Content-Type' => 'application/json' })
+      end
+
+      it 'raises Zoom::Error exception' do
+        expect { zc.account_managed_domains(args) }.to raise_error(Zoom::Error)
+      end
+    end
+  end
+end
