98ee3a593753f47afcf132ab87ee794d5b2d653e
cidsinga <cidsinga@gmail.com>
2020-10-14 15:09:51 -0700

    add delete internal callout country endpoint and specs

--- lib/zoom/actions/sip_audio.rb
+++ lib/zoom/actions/sip_audio.rb
@@ -8,6 +8,18 @@ module Zoom
         params.require(:account_id)
         Utils.parse_response self.class.get("/accounts/#{params[:account_id]}/sip_trunk/trunks", headers: request_headers)
       end
+
+      def sip_trunks_delete(*args)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:account_id, :trunk_id)
+        Utils.parse_response self.class.delete("/accounts/#{params[:account_id]}/sip_trunk/trunks/#{params[:trunk_id]}", headers: request_headers)
+      end
+
+      def sip_trunks_internal_callout_country_delete(*args)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:account_id, :country_id)
+        Utils.parse_response self.class.delete("/accounts/#{params[:account_id]}/sip_trunk/callout_countries/#{params[:country_id]}", headers: request_headers)
+      end
     end
   end
 end
