378454c682b665923ffb78384a67a8e35cb1be93
Kyle Boe <ksboe1@gmail.com>
2020-10-30 17:16:15 -0700

    add webinar action webinar_panelist_list - #317

--- lib/zoom/actions/webinar.rb
+++ lib/zoom/actions/webinar.rb
@@ -85,6 +85,12 @@ module Zoom
         params.require(%i[webinar_id id]).permit(:occurrence_id)
         Utils.parse_response self.class.get("/webinars/#{params[:webinar_id]}/registrants/#{params[:id]}", query: params.except(:webinar_id, :id), headers: request_headers)
       end
+
+      def webinar_panelist_list(*args)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.require(:webinar_id)
+        Utils.parse_response self.class.get("/webinars/#{params[:webinar_id]}/panelists", headers: request_headers)
+      end
     end
   end
 end
