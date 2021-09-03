8caa3543ecef1825dc978a4bc22a05565ec0af68
Joel Hayhurst <joelmichael@gmail.com>
2021-09-03 16:56:39 -0700

    Attempt refactor
    Creates lib/zoom/actions.rb (Zoom::ActionDefinitions).
    Reworks Zoom::Actions::Account to use new define_action method.

---
+++ lib/zoom/actions.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+module Zoom
+  module ActionDefinitions
+    def self.parse_url(url, params)
+      parsed_url = url.dup
+      url.scan(/:\w+/).each do |match|
+        key         = match[1..].to_sym
+        value       = params[key].to_s
+        parsed_url  = parsed_url.gsub(match, value)
+      end
+      parsed_url
+    end
+
+    def self.make_request(obj, method, url, params, required_params)
+      request_options = {
+        headers: obj.request_headers
+      }
+      case method
+      when :get
+        request_options[:query] = required_params
+      when :post, :patch
+        request_options[:body] = required_params.to_json
+      end
+      parsed_url = Zoom::ActionDefinitions.parse_url(url, params)
+      obj.class.public_send(method, parsed_url, **request_options)
+    end
+
+    def define_action(name, method, url, required: [], permitted: [])
+      required  = [required]  if required.is_a?(Symbol)
+      permitted = [permitted] if permitted.is_a?(Symbol)
+
+      define_method(name) do |*args|
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        required_params = required.empty? ? params : params.require(required)
+        required_params.permit(permitted) unless permitted.empty?
+        response = Zoom::ActionDefinitions.make_request(self, method, url, params, required_params)
+        Utils.parse_response(response)
+      end
+    end
+  end
+end
--- lib/zoom/actions/account.rb
+++ lib/zoom/actions/account.rb
@@ -3,65 +3,82 @@
 module Zoom
   module Actions
     module Account
-      def account_list(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.permit(%i[page_size page_number])
-        Utils.parse_response self.class.get('/accounts', query: params, headers: request_headers)
-      end
+      extend Zoom::ActionDefinitions
 
-      def account_create(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(%i[first_name last_name email password]).permit(options: %i[share_rc room_connectors share_mc meeting_connectors pay_mode])
-        Utils.parse_response self.class.post('/accounts', body: params.to_json, headers: request_headers)
-      end
+      def self.included(base)
+        define_action(
+          'account_list',
+          :get,
+          '/accounts',
+          permitted:  %i[page_size page_number]
+        )
 
-      def account_get(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:account_id)
-        Utils.parse_response self.class.get("/accounts/#{params[:account_id]}", headers: request_headers)
-      end
+        define_action(
+          'account_create',
+          :post,
+          '/accounts',
+          required:   %i[first_name last_name email password],
+          permitted:  { options: %i[share_rc room_connectors share_mc meeting_connectors pay_mode] }
+        )
 
-      def account_delete(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:account_id)
-        Utils.parse_response self.class.delete("/accounts/#{params[:account_id]}", headers: request_headers)
-      end
+        define_action(
+          'account_get',
+          :get,
+          '/accounts/:account_id',
+          required: :account_id
+        )
 
-      def account_options_update(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:account_id).permit(%i[share_rc room_connectors share_mc meeting_connectors pay_mode])
-        Utils.parse_response self.class.patch("/accounts/#{params[:account_id]}/options", body: params.except(:account_id).to_json, headers: request_headers)
-      end
+        define_action(
+          'account_delete',
+          :delete,
+          '/accounts/:account_id',
+          required: :account_id
+        )
 
-      def account_settings_get(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:account_id).permit(:option)
-        Utils.parse_response self.class.get("/accounts/#{params[:account_id]}/settings", query: params.except(:account_id), headers: request_headers)
-      end
+        define_action(
+          'account_options_update',
+          :patch,
+          '/accounts/:account_id/options',
+          required:   :account_id,
+          permitted:  %i[share_rc room_connectors share_mc meeting_connectors pay_mode]
+        )
 
-      def account_settings_update(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:account_id).permit(:option, Zoom::Constants::Account::Settings::PERMITTED_KEYS)
-        params.permit_value(:option, Zoom::Constants::Account::Settings::PERMITTED_OPTIONS)
-        Utils.parse_response self.class.patch("/accounts/#{params[:account_id]}/settings", query: params.slice(:option), body: params.except(%i[account_id option]).to_json, headers: request_headers)
-      end
+        define_action(
+          'account_settings_get',
+          :get,
+          '/accounts/:account_id/settings',
+          required:   :account_id,
+          permitted:  :option
+        )
 
-      def account_managed_domains(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:account_id)
-        Utils.parse_response self.class.get("/accounts/#{params[:account_id]}/managed_domains", headers: request_headers)
-      end
+        define_action(
+          'account_settings_update',
+          :patch,
+          '/accounts/:account_id/settings',
+          required:   :account_id,
+          permitted:  [:option, Zoom::Constants::Account::Settings::PERMITTED_KEYS]
+        )
 
-      def account_get_locked_settings(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:account_id)
-        Utils.parse_response self.class.get("/accounts/#{params[:account_id]}/lock_settings", headers: request_headers)
-      end
+        define_action(
+          'account_managed_domains',
+          :get,
+          '/accounts/:account_id/managed_domains',
+          required: :account_id
+        )
 
-      def account_trusted_domains(*args)
-        params = Zoom::Params.new(Utils.extract_options!(args))
-        params.require(:account_id)
-        Utils.parse_response self.class.get("/accounts/#{params[:account_id]}/trusted_domains", headers: request_headers)
+        define_action(
+          'account_get_locked_settings',
+          :get,
+          '/accounts/:account_id/lock_settings',
+          required: :account_id
+        )
+
+        define_action(
+          'account_trusted_domains',
+          :get,
+          '/accounts/:account_id/trusted_domains',
+          required: :account_id
+        )
       end
     end
   end
