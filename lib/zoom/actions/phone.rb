45bf1efb1f84719f422f287a8d3d3c7a19334662
Joel Hayhurst <joelmichael@gmail.com>
2021-10-22 14:24:34 -0700

    Rework Phone

--- lib/zoom/actions/phone.rb
+++ lib/zoom/actions/phone.rb
@@ -3,25 +3,25 @@
 module Zoom
   module Actions
     module Phone
-      def call_logs(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args))
-        options.require(%i[user_id])
-        response = self.class.get("/phone/users/#{options[:user_id]}/call_logs", query: options.except(:user_id), headers: request_headers)
-        Utils.parse_response(response)
-      end
+      extend Zoom::Actions
 
-      def phone_users_list(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args))
-        response = self.class.get("/phone/users", query: options, headers: request_headers)
-        Utils.parse_response(response)
-      end
+      define_action(
+        name: 'call_logs',
+        method: :get,
+        url: '/phone/users/:user_id/call_logs'
+      )
 
-      def call_recordings(*args)
-        options = Zoom::Params.new(Utils.extract_options!(args))
-        options.require(%i[user_id])
-        response = self.class.get("/phone/users/#{options[:user_id]}/recordings", query: options.except(:user_id), headers: request_headers)
-        Utils.parse_response(response)
-      end
+      define_action(
+        name: 'phone_users_list',
+        method: :get,
+        url: '/phone/users'
+      )
+
+      define_action(
+        name: 'call_recordings',
+        method: :get,
+        url: '/phone/users/:user_id/recordings'
+      )
     end
   end
 end
