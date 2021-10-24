b50d80859cf263caab488fd5cd915d969a619a5c
Alexandr Kalinka <alex.kalinoff@gmail.com>
2021-10-24 14:05:30 +0300

    #132 - Create a Group

--- lib/zoom/actions/groups.rb
+++ lib/zoom/actions/groups.rb
@@ -7,6 +7,12 @@ module Zoom
         Utils.parse_response self.class.get('/groups', headers: request_headers)
       end
 
+      def group_create(*args)
+        params = Zoom::Params.new(Utils.extract_options!(args))
+        params.permit(%i[name])
+        Utils.parse_response self.class.post("/groups", body: params, headers: request_headers)
+      end
+
       def groups_get(*args)
         params = Zoom::Params.new(Utils.extract_options!(args))
         params.require(:group_id)
