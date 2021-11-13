963afbc39727801eb2149ae13be47d7e2ceb1a02
Joel Hayhurst <joelmichael@gmail.com>
2021-11-12 16:47:14 -0800

    Rename url methods to path
    Add make_request specs.

--- lib/zoom/actions.rb
+++ lib/zoom/actions.rb
@@ -2,45 +2,45 @@
 
 module Zoom
   module Actions
-    def self.extract_url_keys(url)
-      url.scan(/:\w+/).map { |match| match[1..].to_sym }
+    def self.extract_path_keys(path)
+      path.scan(/:\w+/).map { |match| match[1..].to_sym }
     end
 
-    def self.parse_url(url, url_keys, params)
-      parsed_url = url.dup
-      url_keys.each do |key|
+    def self.parse_path(path, path_keys, params)
+      parsed_path = path.dup
+      path_keys.each do |key|
         value       = params[key].to_s
-        parsed_url  = parsed_url.sub(":#{key}", value)
+        parsed_path  = parsed_path.sub(":#{key}", value)
       end
-      parsed_url
+      parsed_path
     end
 
-    def self.make_request(obj, method, parsed_url, params, base_uri)
-      request_options = { headers: obj.request_headers }
+    def self.make_request(client, method, parsed_path, params, base_uri)
+      request_options = { headers: client.request_headers }
       request_options[:base_uri] = base_uri if base_uri
       case method
       when :get
         request_options[:query] = params
-      when :post, :patch
+      when :post, :put, :patch
         request_options[:body] = params.to_json
       end
-      obj.class.public_send(method, parsed_url, **request_options)
+      client.class.public_send(method, parsed_path, **request_options)
     end
 
     [:get, :post, :patch, :put, :delete].each do |method|
-      define_method(method) do |name, url, options={}|
+      define_method(method) do |name, path, options={}|
         required, permitted, base_uri = options.values_at :require, :permit, :base_uri
         required = Array(required) unless required.is_a?(Hash)
         permitted = Array(permitted) unless permitted.is_a?(Hash)
 
         define_method(name) do |*args|
-          url_keys = Zoom::Actions.extract_url_keys(url)
+          path_keys = Zoom::Actions.extract_path_keys(path)
           params = Zoom::Params.new(Utils.extract_options!(args))
-          parsed_url = Zoom::Actions.parse_url(url, url_keys, params)
-          params = params.require(url_keys) unless url_keys.empty?
+          parsed_path = Zoom::Actions.parse_path(path, path_keys, params)
+          params = params.require(path_keys) unless path_keys.empty?
           params_without_required = required.empty? ? params : params.require(required)
           params_without_required.permit(permitted) unless permitted.empty?
-          response = Zoom::Actions.make_request(self, method, parsed_url, params, base_uri)
+          response = Zoom::Actions.make_request(self, method, parsed_path, params, base_uri)
           Utils.parse_response(response)
         end
       end
--- spec/lib/zoom/actions_spec.rb
+++ spec/lib/zoom/actions_spec.rb
@@ -3,21 +3,99 @@
 require 'spec_helper'
 
 describe Zoom::Actions do
-  describe 'self.extract_url_keys' do
-    subject { described_class.extract_url_keys(url) }
+  describe 'self.extract_path_keys' do
+    subject { described_class.extract_path_keys(path) }
 
-    let(:url) { 'https://example.com/:id/foo/:bar' }
+    let(:path) { '/:id/foo/:bar' }
     
     it { is_expected.to contain_exactly(:id, :bar) }
   end
 
-  describe 'self.parse_url' do
-    subject { described_class.parse_url(url, url_keys, params) }
+  describe 'self.parse_path' do
+    subject { described_class.parse_path(path, path_keys, params) }
 
-    let(:url) { 'https://example.com/:id/foo/:bar' }
-    let(:url_keys) { [:id, :bar] }
+    let(:path) { '/:id/foo/:bar' }
+    let(:path_keys) { [:id, :bar] }
     let(:params) { { id: 100, bar: 'baz' } }
 
-    it { is_expected.to eq('https://example.com/100/foo/baz') }
+    it { is_expected.to eq('/100/foo/baz') }
+  end
+
+  describe 'self.make_request' do
+    subject { described_class.make_request(client, method, parsed_path, params, base_uri) }
+
+    let(:client) { Zoom::Client::OAuth.new(access_token: 'xxx', timeout: 15) }
+    let(:parsed_path) { '/100/foo/baz' }
+    let(:params) { { id: 100, bar: 'baz' } }
+    let(:base_uri) { 'https://example.com' }
+
+    context 'when get' do
+      let(:method) { :get }
+
+      it 'calls get method on client with get request_options' do
+        request_options = {
+          headers: client.request_headers,
+          query: params,
+          base_uri: base_uri
+        }
+        expect(client.class).to receive(method).with(parsed_path, request_options)
+        subject
+      end
+    end
+
+    context 'when post' do
+      let(:method) { :post }
+
+      it 'calls post method on client with post request_options' do
+        request_options = {
+          headers: client.request_headers,
+          body: params.to_json,
+          base_uri: base_uri
+        }
+        expect(client.class).to receive(method).with(parsed_path, request_options)
+        subject
+      end
+    end
+
+    context 'when put' do
+      let(:method) { :put }
+
+      it 'calls put method on client with put request_options' do
+        request_options = {
+          headers: client.request_headers,
+          body: params.to_json,
+          base_uri: base_uri
+        }
+        expect(client.class).to receive(method).with(parsed_path, **request_options)
+        subject
+      end
+    end
+
+    context 'when patch' do
+      let(:method) { :patch }
+
+      it 'calls patch method on client with patch request_options' do
+        request_options = {
+          headers: client.request_headers,
+          body: params.to_json,
+          base_uri: base_uri
+        }
+        expect(client.class).to receive(method).with(parsed_path, **request_options)
+        subject
+      end
+    end
+
+    context 'when delete' do
+      let(:method) { :delete }
+
+      it 'calls delete method on client with delete request_options' do
+        request_options = {
+          headers: client.request_headers,
+          base_uri: base_uri
+        }
+        expect(client.class).to receive(method).with(parsed_path, **request_options)
+        subject
+      end
+    end
   end
 end
