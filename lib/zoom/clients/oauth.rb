78f1bfffe8f9f2edbbd67800adf0908fd882c7fc
Nikita Popov <Nikita.Popov@itechart-group.com>
2022-01-25 11:34:32 +0300

    Permit code_verifier parameter

--- lib/zoom/clients/oauth.rb
+++ lib/zoom/clients/oauth.rb
@@ -13,7 +13,7 @@ module Zoom
       # Returns (access_token, refresh_token)
       #
       def initialize(config)
-        Zoom::Params.new(config).permit( %i[auth_token auth_code redirect_uri access_token refresh_token timeout])
+        Zoom::Params.new(config).permit( %i[auth_token auth_code redirect_uri access_token refresh_token timeout code_verifier])
         Zoom::Params.new(config).require_one_of(%i[access_token refresh_token auth_code])
         if (config.keys & [:auth_code, :redirect_uri]).any?
           Zoom::Params.new(config).require( %i[auth_code redirect_uri])
